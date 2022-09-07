35cf1495178baee5c25a1c014f8434b772f31594
iesusdavila <irdavila@espol.edu.ec>
2022-09-07 18:12:46 -0500

    Sistema de vidas y niveles implementados

--- game.js
+++ game.js
@@ -8,10 +8,24 @@ const btnright = document.querySelector("#right");
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
+window.addEventListener("keydown", moveByKeys);
+btnup.addEventListener("click", moveUp);
+btndown.addEventListener("click", moveDown);
+btnleft.addEventListener("click", moveLeft);
+btnright.addEventListener("click", moveRight);
+
 let canvasSize;
 let elementsSize;
+let level = 0;
+let lives = 3;
+let enemiesPositions = [];
 
 const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
+const giftPosition = {
   x: undefined,
   y: undefined,
 };
@@ -20,12 +34,24 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "right";
 
-  const map = maps[0];
+  const map = maps[level];
+  if (!map) {
+    playerWin();
+    return;
+  }
   const arraymap = map.trim().split("\n");
 
   arraymap.forEach(
     (fila, indFila) => (arraymap[indFila] = fila.trim().split(""))
   );
+
+  const enemiesArray = [];
+  let enemiesTotal = 0;
+
+  arraymap.forEach((fila, indFila) => {
+    enemiesArray[indFila] = fila.filter((value) => value == "X");
+    enemiesTotal += enemiesArray[indFila].length;
+  });
 
   game.clearRect(0, 0, canvasSize, canvasSize);
 
@@ -39,6 +65,17 @@ function startGame() {
         playerPosition.y = posY;
       }
 
+      if (caracter == "I" && !giftPosition.x && !giftPosition.y) {
+        giftPosition.x = posX;
+        giftPosition.y = posY;
+      }
+
+      if (caracter == "X" && enemiesPositions.length < enemiesTotal) {
+        enemiesPositions.push({
+          x: posX,
+          y: posY,
+        });
+      }
       game.fillText(emojis[caracter], posX, posY);
     });
   });
@@ -59,11 +96,65 @@ function setCanvasSize() {
   startGame();
 }
 
-window.addEventListener("keydown", moveByKeys);
-btnup.addEventListener("click", moveUp);
-btndown.addEventListener("click", moveDown);
-btnleft.addEventListener("click", moveLeft);
-btnright.addEventListener("click", moveRight);
+function movePlayer() {
+  const giftCollisionX =
+    playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+  const giftCollisionY =
+    playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+
+  if (giftCollisionX && giftCollisionY) {
+    nextLevel();
+  }
+
+  const enemyCollision = enemiesPositions.find((enemy) => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+    return enemyCollisionX && enemyCollisionY;
+  });
+
+  if (enemyCollision) {
+    lives--;
+    if (lives == 0) {
+      playerLoser();
+      lives = 3;
+    } else {
+      restoreLevel();
+    }
+  }
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function restoreLevel() {
+  restoreEnemyAndGift();
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+  startGame();
+}
+
+function playerLoser() {
+  level = 0;
+  restoreEnemyAndGift();
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+  startGame();
+}
+
+function nextLevel() {
+  level++;
+  restoreEnemyAndGift();
+  startGame();
+}
+
+function restoreEnemyAndGift() {
+  giftPosition.x = undefined;
+  giftPosition.y = undefined;
+  enemiesPositions = [];
+  enemiesTotal = 0;
+}
+
+function playerWin() {
+  console.log("GANO ESTE PELAVERGA");
+}
 
 function moveByKeys(event) {
   switch (event.code) {
@@ -101,7 +192,7 @@ function moveByKeys(event) {
 }
 
 function moveUp() {
-  if (!(playerPosition.y - elementsSize < elementsSize)) {
+  if (!(playerPosition.y - elementsSize < 0)) {
     playerPosition.y -= elementsSize;
   }
   startGame();
@@ -126,8 +217,4 @@ function moveRight() {
     playerPosition.x += elementsSize;
   }
   startGame();
-}
-
-function movePlayer() {
-  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 }
